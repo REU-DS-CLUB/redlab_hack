be5a0b4f5652c9c8a97d1d4e5c0ca1923987dae6
Lesha <aleksvkv95@gmail.com>
2024-05-19 10:11:36 +0300

    add file_uplouder

--- pages/analysis.py
+++ pages/analysis.py
@@ -1,7 +1,19 @@
 import streamlit as st
+import pandas as pd
 st.set_page_config(
     page_title='analysis_time_series',
     page_icon='📊'
 )
 st.sidebar.success("Выберете интересующий раздел")
-data_file = st.file_uploader("Загрузите данные временного ряда CSV файлом")
+st.header('Анализ временного ряда')
+data_file = st.file_uploader(label='')
+if data_file is not None:
+    try:
+        # Пытаемся загрузить файл как CSV
+        data = pd.read_csv(data_file)
+        st.success("Файл успешно загружен!")
+
+    except Exception as e:
+        st.error("Ошибка при загрузке файла: убедитесь, что файл имеет формат .csv")
+else:
+    st.warning("Пожалуйста, загрузите CSV файл для анализа.")
