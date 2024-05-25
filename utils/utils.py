1a432e9f7848b71487a25801c99ec678342f5346
Александр <lild1tz2023@gmail.com>
2024-05-25 11:13:01 +0300

    func with metrics in dict

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def web_response(data: pd.DataFrame) -> pd.DataFrame:
@@ -88,3 +89,21 @@ def error(data: pd.DataFrame) -> pd.DataFrame:
     result_data = merged_data.sort_values(by='time')
 
     return result_data[["time", "metric"]]
+
+def metrics(df: pd.DataFrame, mask: np.array) -> dict:
+
+    normalized_metric = (df['metric'] - np.mean(df['metric'])) / np.std(df['metric'])
+    original_variance = np.var(normalized_metric)
+    filtered_metric = normalized_metric[mask == 0]
+    filtered_variance = np.var(filtered_metric)
+
+    variance_difference = filtered_variance / original_variance * 100 - 100
+    fraction_anomaly = len(mask[mask == 1]) / len(mask)
+
+    metric = {
+        "variance_diff": variance_difference,
+        "fraction_anomaly": fraction_anomaly
+    }
+
+    return metric
+
