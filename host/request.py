342e46227ff2e7c5edd7b6e97906889519e080f6
Maksoit <maksim200425@yandex.ru>
2024-05-26 05:03:25 +0300

    a;dksjf

--- host/request.py
+++ host/request.py
@@ -1,7 +1,17 @@
 import requests
 
 def get_marc(start_dt, end_dt):
-    url = f"80.87.104.231:8000/handlers/simple?start_dt={start_dt}&end_dt={end_dt}"
+    url = f"http://80.87.104.231:8000/handlers/global_detection?start_dt={start_dt}&end_dt={end_dt}"
     
     response = requests.get(url, verify= False)
+    print("*"*100)
+    print(response)
+    return response
+
+def get_new_anomalies(start_dt, end_dt):
+    url = f"http://80.87.104.231:8000/handlers/local_detection?start_dt={start_dt}&end_dt={end_dt}"
+    
+    response = requests.get(url, verify= False)
+    print("*"*100)
+    print(response)
     return response
--- main.py
+++ main.py
@@ -8,3 +8,4 @@ show_pages(
     ]
 )
 st.sidebar.success("Выберете интересующий раздел")
+st.experimental_rerun()
